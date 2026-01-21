22ca68f90d2afacdfc61487e7b72dcc542328a9c
rmc-8.com <kmprmc@gmail.com>
2026-01-22 05:43:15 +0900

    feat: Polarsのリスト、配列、構造体、日付時刻、および値カウントの新しい使用例を追加

--- src/ch12.py
+++ src/ch12.py
@@ -78,6 +78,160 @@ def _(pl):
 
 
 @app.cell
+def _(pl):
+    pl.DataFrame(
+        {
+            "utc_mixed_offset": [
+                "2021-03-27T00:00:00+0100",
+                "2021-03-28T00:00:00+0100",
+                "2021-03-29T00:00:00+0200",
+                "2021-03-30T00:00:00+0200",
+            ]
+        }
+    ).with_columns(
+        parsed=pl.col("utc_mixed_offset").str.to_datetime("%Y-%m-%dT%H:%M:%S%z")
+    ).with_columns(converted=pl.col("parsed").dt.convert_time_zone("Europe/Amsterdam"))
+    return
+
+
+@app.cell
+def _(pl):
+    bools = pl.DataFrame({"values": [[True, True], [False, False, True], [False]]})
+    bools.with_columns(
+        all_true=pl.col("values").list.all(),
+        any_true=pl.col("values").list.any(),
+    )
+    return
+
+
+@app.cell
+def _(pl):
+    groups = pl.DataFrame({"ages": [[18, 21], [30, 40, 50], [42, 69]]})
+    groups.with_columns(
+        over_forty=pl.col("ages").list.eval(
+            pl.element() > 40,
+            parallel=True,
+        )
+    ).with_columns(all_over_forty=pl.col("over_forty").list.all())
+    return (groups,)
+
+
+@app.cell
+def _(groups, pl):
+    groups.with_columns(
+        ages_sorted_descending=pl.col("ages").list.sort(descending=True)
+    )
+    return
+
+
+@app.cell
+def _(groups):
+    groups.explode("ages")
+    return
+
+
+@app.cell
+def _(groups, pl):
+    groups.select(ages=pl.col("ages").list.explode())
+    return
+
+
+@app.cell
+def _(pl):
+    events = pl.DataFrame(
+        [
+            pl.Series("location", ["Paris", "Amsterdam", "Barcelona"], dtype=pl.String),
+            pl.Series(
+                "temparatures",
+                [
+                    [23, 27, 21, 22, 24, 23, 22],
+                    [17, 19, 15, 22, 18, 20, 21],
+                    [30, 32, 28, 29, 34, 33, 31],
+                ],
+                dtype=pl.Array(pl.Int64, shape=7),
+            ),
+        ]
+    )
+    events
+    return (events,)
+
+
+@app.cell
+def _(events, pl):
+    events.with_columns(
+        median=pl.col("temparatures").arr.median(),
+        max=pl.col("temparatures").arr.max(),
+        warmest_dow=pl.col("temparatures").arr.arg_max(),
+    )
+    return
+
+
+@app.cell
+def _(pl):
+    from datetime import date
+
+    orders = pl.DataFrame(
+        {
+            "customer_id": [2781, 6139, 5392],
+            "order_details": [
+                {"amount": 250.00, "date": date(2024, 1, 3), "items":5},
+                {"amount": 150.00, "date": date(2024, 1, 5), "items": 1},
+                {"amount": 100.00, "date": date(2024, 1, 2), "items": 3},
+            ]
+        }
+    )
+    orders
+    return (orders,)
+
+
+@app.cell
+def _(orders, pl):
+    orders.select(pl.col("order_details").struct.field("amount"))
+    return
+
+
+@app.cell
+def _(orders):
+    order_details_df = orders.unnest("order_details")
+    order_details_df
+    return (order_details_df,)
+
+
+@app.cell
+def _(order_details_df, pl):
+    order_details_df.select(
+        "amount",
+        "date",
+        "items",
+        order_details=pl.struct(pl.col("amount"), pl.col("date"), pl.col("items")),
+    )
+    return
+
+
+@app.cell
+def _(pl):
+    basket = pl.DataFrame(
+        {
+            "fruit": ["cherry", "apple", "banana", "banana", "apple", "banana"],
+        }
+    )
+    basket
+    return (basket,)
+
+
+@app.cell
+def _(basket, pl):
+    basket.select(pl.col("fruit").value_counts(sort=True))
+    return
+
+
+@app.cell
+def _(basket, pl):
+    basket.select(pl.col("fruit").value_counts(sort=True).struct.unnest())
+    return
+
+
+@app.cell
 def _():
     return
 
